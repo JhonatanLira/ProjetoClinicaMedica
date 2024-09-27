76b3afa9f4edc9e2c4153e3c5955fe18b0384eaf
Jhonatan <jhonatanlira@gmail.com>
2024-09-27 16:47:37 -0300

    CRUD Medico

---
+++ src/main/java/com/clinica/api/controller/MedicoController.java
@@ -0,0 +1,62 @@
+package com.clinica.api.controller;
+
+import com.clinica.domain.model.Medico;
+import com.clinica.domain.repository.MedicoRepository;
+import com.clinica.domain.service.MedicoService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "Medico", description = "CRUD Médico")
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoService medicoService;
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @GetMapping
+    public List<Medico> listar() {
+        return medicoService.listar();
+    }
+
+    @GetMapping("/{idMedico}")
+    public ResponseEntity<Medico> buscaPorId(@PathVariable Long idMedico) {
+        if (medicoRepository.findById(idMedico).isPresent()) {
+            return ResponseEntity.ok(medicoService.buscarMedico(idMedico));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Medico> salvar(@RequestBody @Valid Medico medico) {
+        medicoService.salvar(medico);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/{idMedico}")
+    public ResponseEntity<Medico> atualizar(@RequestBody Medico medicoAtualizado, @PathVariable Long idMedico) {
+        if (medicoRepository.findById(idMedico).isPresent()) {
+            medicoService.atualizar(idMedico, medicoAtualizado);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @DeleteMapping("/{idMedico}")
+    public ResponseEntity<Medico> excluir(@PathVariable Long idMedico) {
+        if (medicoRepository.findById(idMedico).isPresent()) {
+            medicoService.deletar(idMedico);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+}
---
+++ src/main/java/com/clinica/domain/service/impl/MedicoServiceImpl.java
@@ -0,0 +1,44 @@
+package com.clinica.domain.service.impl;
+
+import com.clinica.domain.model.Medico;
+import com.clinica.domain.repository.MedicoRepository;
+import com.clinica.domain.service.MedicoService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MedicoServiceImpl implements MedicoService {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Override
+    public List<Medico> listar() {
+        return medicoRepository.findAll();
+    }
+
+    @Override
+    public Medico buscarMedico(Long idMedico) {
+        return medicoRepository.findById(idMedico).get();
+    }
+
+    @Override
+    public void salvar(Medico medico) {
+        medicoRepository.save(medico);
+    }
+
+    @Override
+    public void atualizar(Long id, Medico medicoAtualizado) {
+        Medico medico = medicoRepository.findById(id).get();
+        BeanUtils.copyProperties(medicoAtualizado, medico, "id");
+        salvar(medico);
+    }
+
+    @Override
+    public void deletar(Long idMedico) {
+        medicoRepository.findById(idMedico);
+    }
+}
